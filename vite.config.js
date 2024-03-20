0cb85d54af15f68dbf107093b32c7027fc010c9d
glaures <guido@laures.de>
2024-03-20 11:26:30 +0100

    added dist

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/spaboot-service-frontend/",
   plugins: [
     vue(),
     VueDevTools(),
