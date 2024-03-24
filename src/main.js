0ba9321884d70db3bbb82f830545027aab15ea05
glaures <guido@laures.de>
2024-03-24 17:11:06 +0100

    registerServiceWorker.js

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import router from './router'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {faCircleInfo} from "@fortawesome/free-solid-svg-icons";
+import './registerServiceWorker'
 library.add(faCircleInfo)
 
 const app = createApp(App)
